4dc1874acbb420b6c047ce84d8a83152672ccf57
Nithin K M <45710377+Nithin18@users.noreply.github.com>
2024-02-03 22:06:12 +0530

    Added command to create network bridge to communicate b/w two containers

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -5,14 +5,19 @@ source .env
 
 TAG="movie-rental"
 
+docker rm $TAG
+
 docker build -t "$TAG" .
 
+docker network create bridge_for_movie_rental
+
 docker run \
+  --name="$TAG" \
+  --network=bridge_for_movie_rental \
   -e DB_HOST="$DB_HOST" \
   -e DB_PORT="$DB_PORT" \
   -e DB_USER="$DB_USER" \
   -e DB_PASSWORD="$DB_PASSWORD" \
   -e DB_NAME="$DB_NAME" \
+  -p 8080:8080 \
   -t "$TAG"
-
-
